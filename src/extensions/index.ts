75d451f8aea5c2ad885e4261fafb3d0fbe6a0d30
Jerry <x.jerry.wang@gmail.com>
2026-08-13 20:08:17 +0800

    chore: disable tool gate

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -7,8 +7,15 @@ import registerAskExtension from './ask/index.ts'
 import registerSubAgentExtension from './agents/index.ts'
 import registerToolGateExtension from './tool-gate/index.ts'
 
+const CONFIG = {
+  enableToolGate: false
+}
+
 export default function (pi: ExtensionAPI) {
   registerSubAgentExtension(pi)
   registerAskExtension(pi)
-  // registerToolGateExtension(pi)
+
+  if (CONFIG.enableToolGate) {
+    registerToolGateExtension(pi)
+  }
 }
